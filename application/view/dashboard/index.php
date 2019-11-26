c93d3a6ffeea4829ca9639543612148ccc41fd9d
Ravenstoner <40850544+Ravenstoner@users.noreply.github.com>
2019-11-26 14:44:50 +0100

    test commit

--- application/view/dashboard/index.php
+++ application/view/dashboard/index.php
@@ -5,7 +5,7 @@
         <!-- echo out the system feedback (error and success messages) -->
         <?php $this->renderFeedbackMessages(); ?>
 
-        <h3>What happens here ?</h3>
+        <h3>Startorano</h3>
         <p>
             This is an area that's only visible for logged in users. Try to log out, an go to /dashboard/ again. You'll
             be redirected to /index/ as you are not logged in. You can protect a whole section in your app within the
