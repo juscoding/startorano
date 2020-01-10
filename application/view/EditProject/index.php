53fd8981bb982a17cc24acb8ef8e978e2dae63a2
juscoding <47847734+juscoding@users.noreply.github.com>
2020-01-10 18:36:26 +0100

    Anzeige bearbeiten
    Basis Anzeige bearbeiten

---
+++ application/view/EditProject/index.php
@@ -0,0 +1,108 @@
+    <!-- Header Datei einbinden -->
+    <?php include("../application/view/_templates/header_standard.php"); ?>
+
+    <form class="wrapper" action="<?php echo Config::get('URL'); ?>EditProject/createNewAnzeige" method="post">
+        <!-- <input  type="hidden" name="anzeigen_id" value="4"> -->
+
+        <!-- User Info Element START -->
+
+        <div class="startoranoUserComponentsContainer startoranoUserComponentTypeUserInfo">
+            <div class="startoranoUserComponentTypeUserInfoRow1">
+                <img src="<?php echo Config::get('URL'); ?>/images/profilePic2.png" alt="ProfilePicture">
+            </div>
+            <div class="startoranoUserComponentTypeUserInfoRow2">
+                <div>
+
+                    <p><?= $this->user_name; ?></p>
+                    <!-- <p><?= print_r($this->editProject); ?></p> -->
+                </div>
+                <div>
+                    <small>Programmer</small>
+                </div>
+            </div>
+        </div>
+
+        <input type="hidden" name="anzeigen_auftraggeber" value="<?= $this->user_name; ?>">
+
+        <!-- User Info Element END -->
+
+        <p class="startoranoNeueAnzeigeZwischentext">sucht</p>
+
+        <!-- SerachInput START -->
+
+        <div class="startoranoUserComponentsContainer startoranoUserComponentTypeSearch">
+            <div class="startoranoUserComponentTypeSearchListElementMain">
+                <input type="text" controller="add" name="getJobs" placeholder="Job wählen..." autocomplete="off" value="<?php echo $this->editProject[0]->Jobbezeichnung ?>" required>
+                <img src="<?php echo Config::get('URL'); ?>images/svg/searchIcon.svg" alt="searchIcon">
+                <input type="hidden" name="anzeigen_jobId">
+            </div>
+            <div class="startoranoUserComponentTypeSearchListElementWrapper"></div>
+        </div>
+        <!-- SerachInput ENDE -->
+
+        <p class="startoranoNeueAnzeigeZwischentext">für</p>
+
+        <!-- TextInput START -->
+        <div class="startoranoUserComponentsContainer startoranoUserComponentTypeText">
+            <input type="text" name="anzeigen_titel" value="<?php echo $this->editProject[0]->Titel ?>" required>
+        </div>
+        <!-- TextInput END -->
+
+        <!-- TextInput START -->
+        <div class="startoranoUserComponentsContainer startoranoUserComponentTypeText">
+            <textarea rows="5" name="anzeigen_beschreibung" placeholder="Beschreibung..." required><?php echo $this->editProject[0]->Beschreibung ?></textarea>
+        </div>
+        <!-- TextInput END -->
+
+
+        <p class="startoranoNeueAnzeigeZwischentext">angenommen von</p>
+
+        <!-- UserSearch START -->
+        <div class="startoranoUserComponentsContainer startoranoUserComponentTypeSearch">
+            <div class="startoranoUserComponentTypeSearchListElementMain">
+                <input type="text" controller="NewChat" name="message_recipient" placeholder="Bearbeiter" value="<?php echo $this->editProject[0]->user_name ?>" autocomplete="off">
+                <img src="<?php echo Config::get('URL'); ?>images/svg/searchIcon.svg" alt="searchIcon">
+            </div>
+            <div class="startoranoUserComponentTypeSearchListElementWrapper"></div>
+        </div>
+        <!-- UserSearch ENDE -->
+
+        <!-- DropDownInput START -->
+        <div class="startoranoUserComponentsContainer startoranoUserComponentTypeDropDown">
+            <div class="startoranoUserComponentTypeDropDownListElement startoranoUserComponentTypeDropDownListElementFirst closed">
+                <input type="hidden" name="anzeigen_status">
+                <p><?php
+                    switch ($this->editProject[0]->Status) {
+                        case 0: ?> Status
+                            <?php break; ?>
+                        <?php
+                        case 1: ?> in Bearbeitung
+                            <?php break; ?>
+                        <?php
+                        case 2: ?> erledigt
+                            <?php break; ?>
+                    <?php } ?>
+                </p>
+                <img src="<?php echo Config::get('URL'); ?>images/svg/arrowDownIcon.svg" alt="arrowDownIcon">
+            </div>
+            <div class="startoranoUserComponentTypeDropDownListElement startoranoUserComponentTypeDropDownListElementLoaded">
+                <p>erledigt</p>
+            </div>
+            <div class="startoranoUserComponentTypeDropDownListElement startoranoUserComponentTypeDropDownListElementLoaded">
+                <p>in Bearbeitung</p>
+            </div>
+            <div class="startoranoUserComponentTypeDropDownListElement startoranoUserComponentTypeDropDownListElementLoaded">
+                <p>offen</p>
+            </div>
+        </div>
+        <!-- DropDownInput END -->
+
+
+        <!-- pinker Button mit weißer Schrift -->
+        <input class="startoranoUserComponentsContainer startoranoUserComponentTypeBigButton filledRed" type="submit" value="Speichern">
+
+    </form>
+    <div class="startoranoDeviderFooter"></div>
+
+    <!-- Footer Datei einbinden -->
+    <?php include("../application/view/_templates/footer_standard.php"); ?>
